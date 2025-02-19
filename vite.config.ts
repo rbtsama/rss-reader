280c9e4f7dc837487111f5c5aa41916ae2f76bc7
rbtsama <rbtsama@gmail.com>
2025-02-20 02:03:17 +0800

    fix: update build config for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
+  },
+  build: {
+    sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1500
   }
 }) 
